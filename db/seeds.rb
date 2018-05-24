3d16c64bd95647eae95784422a531a5057349ddc
Antoine Quellier <antoinecquellier@gmail.com>
2018-05-24 09:33:09 +0100

    Models, validations and relationships for cococktails, ingredients and doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+p 'Cleaning database...'
+Ingredient.destroy_all
+
+INGREDIENTS_URL = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_json = open(INGREDIENTS_URL).read
+# .read extract the json file & accesses the ingredients_json
+ingredients_parsed = JSON.parse(ingredients_json)
+# .parse will transform from json to a hash
+
+ingredients = []
+
+ingredients_parsed["drinks"].each do |ingredient|
+  ingredients << Ingredient.create(name: ingredient.values.first)
+end
+p 'Ingredients have been successfully created!'
